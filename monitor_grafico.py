35e4629c4600f2af266bcf57b23196b5a8c79a9c
Adrian Rodriguez <garapower26@gmail.com>
2024-01-15 00:43:38 -0600

    codigo modificado

--- monitor_grafico.py
+++ monitor_grafico.py
@@ -7,7 +7,7 @@ class RouterConfigApp:
     def __init__(self, master):
         self.master = master
         self.master.title("Configuración de Router")
-        
+
         self.label_ip = tk.Label(master, text="Dirección IP del router:")
         self.label_ip.pack()
 
@@ -90,10 +90,10 @@ class RouterConfigApp:
         elif option == "Configuración NAT":
             result = nat_config(ip_router, username, password)
         elif option == "Configuración interfaces":
-            result =interface_config(ip_router, username, password)
+            result = interface_config(ip_router, username, password)
         elif option == "Configuracion RMON":
             result = rmon_config(ip_router, username, password)
-        
+
         self.text_result.delete(1.0, tk.END)
         self.text_result.insert(tk.END, result)
 
@@ -117,276 +117,147 @@ def global_config(router_ip, username, password):
         output = tn.read_until(b"#") #Cambia el prompt según el router
         if b"incorrect" in output or b"denied" in output or b"%" in output:
             Messagebox.showerror("Error", "Error en la autenticacion. Verifica el nombre de usuario y la contraseña.")
-            # print("Error en la autenticacion. Verifica el nombre de usuario y la contraseña.")
         else:
             Messagebox.showinfo("Mensaje", "Conexion exitosa del router.")
-            # print("Conexion exitosa del router.")
-
-            #Enviar el comando "show ip interfaces brief"
             tn.write(b"terminal length 0\n")
             output = tn.read_until(b"#") #Cambia el prompt según el router
             tn.write(b"show running-config\n")
-            #Leer y mostrar la salida del comando
             output = tn.read_until(b"#")
-            #output = tn.read_until(b"!\n!\nend") #Cambia el prompt segun el router
             output = output.decode('ascii')
-            # print(output.decode('ascii'))
-            # Messagebox.showinfo("Datos", output)
-
-
-        #Cerrrar la conexion Telnet
-        tn.close()
 
     except Exception as e:
         Messagebox.showerror("Error al conectarse con el router", str(e))
-        # print("Error al conectarse al router:", str(e))
     return output
 
 def route_config(router_ip, username, password):
-
     try:
-        #Crear una conexión Telnet
         tn = telnetlib.Telnet(router_ip)
-
-        #Esperar a que aparezca el prompt de inicion de sesión
         tn.read_until(b"Username: ")
         tn.write(username.encode('ascii') + b"\n")
-
-        #Espeara a que aprezca el prompt de contraseña
         tn.read_until(b"Password: ")
         tn.write(password.encode('ascii') + b"\n")
-
-        #Verificar si la autentificación fue exitosa
-        output = tn.read_until(b"#") #Cambia el prompt según el router
+        output = tn.read_until(b"#")
         if b"incorrect" in output or b"denied" in output or b"%" in output:
             Messagebox.showerror("Error", "Error en la autenticacion. Verifica el nombre de usuario y la contraseña.")
-            # print("Error en la autenticacion. Verifica el nombre de usuario y la contraseña.")
         else:
             Messagebox.showinfo("Mensaje", "Conexion exitosa del router.")
-            # print("Conexion exitosa del router.")
-            #Enviar el comando "show ip interfaces brief"
             tn.write(b"terminal length 0\n")
-            output = tn.read_until(b"#") 
+            output = tn.read_until(b"#")
             tn.write(b"show ip route\n")
-                
-            #Leer y mostrar la salida del comando
-            output = tn.read_until(b"#") #Cambia el prompt segun el router
+            output = tn.read_until(b"#")
             output = output.decode('ascii')
-            # print(output.decode('ascii'))
-
-        #Cerrrar la conexion Telnet
-        tn.close()
 
     except Exception as e:
         Messagebox.showerror("Error al conectarse con el router", str(e))
-        # print("Error al conectarse al router:", str(e))
     return output
 
 def dhcp_config(router_ip, username, password):
-
     try:
-        #Crear una conexión Telnet
         tn = telnetlib.Telnet(router_ip)
-
-        #Esperar a que aparezca el prompt de inicion de sesión
         tn.read_until(b"Username: ")
         tn.write(username.encode('ascii') + b"\n")
-
-        #Espeara a que aprezca el prompt de contraseña
         tn.read_until(b"Password: ")
         tn.write(password.encode('ascii') + b"\n")
-
-        #Verificar si la autentificación fue exitosa
-        output = tn.read_until(b"#") #Cambia el prompt según el router
+        output = tn.read_until(b"#")
         if b"incorrect" in output or b"denied" in output or b"%" in output:
             Messagebox.showerror("Error", "Error en la autenticacion. Verifica el nombre de usuario y la contraseña.")
-            # print("Error en la autenticacion. Verifica el nombre de usuario y la contraseña.")
         else:
             Messagebox.showinfo("Mensaje", "Conexion exitosa del router.")
-            # print("Conexion exitosa del router.")
-
             tn.write(b"terminal length 0\n")
-            output = tn.read_until(b"#") 
-            #Enviar el comando "show ip interfaces brief"
+            output = tn.read_until(b"#")
             tn.write(b"show ip dhcp pool\n")
-            #Leer y mostrar la salida del comando
-            output = tn.read_until(b"#") #Cambia el prompt segun el router
+            output = tn.read_until(b"#")
             output = output.decode('ascii')
-            # print(output.decode('ascii'))
-            Messagebox.showinfo("Datos", output)
-
-        #Cerrrar la conexion Telnet
-        tn.close()
 
     except Exception as e:
         Messagebox.showerror("Error al conectarse con el router", str(e))
-        # print("Error al conectarse al router:", str(e))
     return output
 
 def acl_config(router_ip, username, password):
-
     try:
-        #Crear una conexión Telnet
         tn = telnetlib.Telnet(router_ip)
-
-        #Esperar a que aparezca el prompt de inicion de sesión
         tn.read_until(b"Username: ")
         tn.write(username.encode('ascii') + b"\n")
-
-        #Espeara a que aprezca el prompt de contraseña
         tn.read_until(b"Password: ")
         tn.write(password.encode('ascii') + b"\n")
-
-        #Verificar si la autentificación fue exitosa
-        output = tn.read_until(b"#") #Cambia el prompt según el router
+        output = tn.read_until(b"#")
         if b"incorrect" in output or b"denied" in output or b"%" in output:
             Messagebox.showerror("Error", "Error en la autenticacion. Verifica el nombre de usuario y la contraseña.")
-            # print("Error en la autenticacion. Verifica el nombre de usuario y la contraseña.")
         else:
             Messagebox.showinfo("Mensaje", "Conexion exitosa del router.")
-            # print("Conexion exitosa del router.")
-
             tn.write(b"show ip access-list\n")
-            #Leer y mostrar la salida del comando
-            output = tn.read_until(b"#") #Cambia el prompt segun el router
+            output = tn.read_until(b"#")
             output = output.decode('ascii')
-            # Messagebox.showinfo("Datos", output)
-
-            # print(output.decode('ascii'))
-
-        #Cerrrar la conexion Telnet
-        tn.close()
 
     except Exception as e:
         Messagebox.showerror("Error al conectarse con el router", str(e))
-        # print("Error al conectarse al router:", str(e))
     return output
 
 def nat_config(router_ip, username, password):
-
     try:
-        #Crear una conexión Telnet
         tn = telnetlib.Telnet(router_ip)
-
-        #Esperar a que aparezca el prompt de inicion de sesión
         tn.read_until(b"Username: ")
         tn.write(username.encode('ascii') + b"\n")
-
-        #Espeara a que aprezca el prompt de contraseña
         tn.read_until(b"Password: ")
         tn.write(password.encode('ascii') + b"\n")
-
-        #Verificar si la autentificación fue exitosa
-        output = tn.read_until(b"#") #Cambia el prompt según el router
+        output = tn.read_until(b"#")
         if b"incorrect" in output or b"denied" in output or b"%" in output:
             Messagebox.showerror("Error", "Error en la autenticacion. Verifica el nombre de usuario y la contraseña.")
-            # print("Error en la autenticacion. Verifica el nombre de usuario y la contraseña.")
         else:
             Messagebox.showinfo("Mensaje", "Conexion exitosa del router.")
-            # print("Conexion exitosa del router.")
-
             tn.write(b"terminal length 0\n")
-            output = tn.read_until(b"#") 
-            #Enviar el comando "show ip interfaces brief"
+            output = tn.read_until(b"#")
             tn.write(b"show ip nat translations\n")
-            #Leer y mostrar la salida del comando
-            output = tn.read_until(b"#") #Cambia el prompt segun el router
+            output = tn.read_until(b"#")
             output = output.decode('ascii')
-            # Messagebox.showinfo("Datos", output)
-            # print(output.decode('ascii'))
-
-        #Cerrrar la conexion Telnet
-        tn.close()
 
     except Exception as e:
         Messagebox.showerror("Error al conectarse con el router", str(e))
-        # print("Error al conectarse al router:", str(e))
     return output
 
 def interface_config(router_ip, username, password):
     try:
-        #Crear una conexión Telnet
         tn = telnetlib.Telnet(router_ip)
-
-        #Esperar a que aparezca el prompt de inicion de sesión
         tn.read_until(b"Username: ")
         tn.write(username.encode('ascii') + b"\n")
-
-        #Espeara a que aprezca el prompt de contraseña
         tn.read_until(b"Password: ")
         tn.write(password.encode('ascii') + b"\n")
-
-        #Verificar si la autentificación fue exitosa
-        output = tn.read_until(b"#") #Cambia el prompt según el router
+        output = tn.read_until(b"#")
         if b"incorrect" in output or b"denied" in output or b"%" in output:
             Messagebox.showerror("Error", "Error en la autenticacion. Verifica el nombre de usuario y la contraseña.")
-            # print("Error en la autenticacion. Verifica el nombre de usuario y la contraseña.")
         else:
             Messagebox.showinfo("Mensaje", "Conexion exitosa del router.")
-            # print("Conexion exitosa del router.")
-
-            #Enviar el comando "show ip interfaces brief"
             tn.write(b"terminal length 0\n")
-            output = tn.read_until(b"#") #Cambia el prompt según el router
-            tn.write(b"show ip int br\n")
-            #Leer y mostrar la salida del comando
             output = tn.read_until(b"#")
-            #output = tn.read_until(b"!\n!\nend") #Cambia el prompt segun el router
+            tn.write(b"show ip int br\n")
+            output = tn.read_until(b"#")
             output = output.decode('ascii')
-            # print(output.decode('ascii'))
-            # Messagebox.showinfo("Datos", output)
-
-
-        #Cerrrar la conexion Telnet
-        tn.close()
 
     except Exception as e:
         Messagebox.showerror("Error al conectarse con el router", str(e))
-        # print("Error al conectarse al router:", str(e))
     return output
 
 def rmon_config(router_ip, username, password):
     try:
-        #Crear una conexión Telnet
         tn = telnetlib.Telnet(router_ip)
-
-        #Esperar a que aparezca el prompt de inicion de sesión
         tn.read_until(b"Username: ")
         tn.write(username.encode('ascii') + b"\n")
-
-        #Espeara a que aprezca el prompt de contraseña
         tn.read_until(b"Password: ")
         tn.write(password.encode('ascii') + b"\n")
-
-        #Verificar si la autentificación fue exitosa
-        output = tn.read_until(b"#") #Cambia el prompt según el router
+        output = tn.read_until(b"#")
         if b"incorrect" in output or b"denied" in output or b"%" in output:
             Messagebox.showerror("Error", "Error en la autenticacion. Verifica el nombre de usuario y la contraseña.")
-            # print("Error en la autenticacion. Verifica el nombre de usuario y la contraseña.")
         else:
             Messagebox.showinfo("Mensaje", "Conexion exitosa del router.")
-            # print("Conexion exitosa del router.")
-
-            #Enviar el comando "show ip interfaces brief"
             tn.write(b"terminal length 0\n")
-            output = tn.read_until(b"#") #Cambia el prompt según el router
-            tn.write(b"show rmon events\n")
-            #Leer y mostrar la salida del comando
             output = tn.read_until(b"#")
-            #output = tn.read_until(b"!\n!\nend") #Cambia el prompt segun el router
+            tn.write(b"show rmon events\n")
+            output = tn.read_until(b"#")
             output = output.decode('ascii')
-            # print(output.decode('ascii'))
-            # Messagebox.showinfo("Datos", output)
-
-
-        #Cerrrar la conexion Telnet
-        tn.close()
 
     except Exception as e:
         Messagebox.showerror("Error al conectarse con el router", str(e))
-        # print("Error al conectarse al router:", str(e))
     return output
-
 
 if __name__ == "__main__":
     root = tk.Tk()
